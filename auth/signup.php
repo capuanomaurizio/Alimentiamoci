e639b8723250c131868ce6190ab00a235d37e14e
capuanomaurizio <mauricapuano@gmail.com>
2022-05-04 10:15:41 +0200

    Rinominazione nomi tabelle in case-insensitive

--- auth/signup.php
+++ auth/signup.php
@@ -4,7 +4,7 @@ $_SESSION["quest2"] = $_POST;
 try {
     //connessione al database
     require("../conn.php");
-    $sql = 'INSERT INTO Utenti VALUES (?,?,?,?,?,?,?,?,?,?)';
+    $sql = 'INSERT INTO utenti VALUES (?,?,?,?,?,?,?,?,?,?)';
     if(isset($_POST["telefono"])){
         $conn->prepare($sql)->execute([$_SESSION["dati"]["username"], $_SESSION["dati"]["password1"], $_SESSION["dati"]["email1"], $_SESSION["dati"]["telefono"], 
     
@@ -18,7 +18,7 @@ try {
         $_SESSION["quest2"]["lifeStyle"], $_SESSION["quest1"]["eta"], $_SESSION["quest1"]["peso"], $_SESSION["quest1"]["fisicoAttuale"], $_SESSION["quest2"]["fisicoIdeale"]]);
     }
     foreach ($_POST["intolleranze"] as $intolleranza) {
-        $sql = 'INSERT INTO IntolleranzeUtenti(nomeintolleranza, username) VALUES (?,?)';
+        $sql = 'INSERT INTO intolleranzeutenti(nomeintolleranza, username) VALUES (?,?)';
         $conn->prepare($sql)->execute([$intolleranza, $_SESSION["dati"]["username"]]);
     }
     $_SESSION["user"] = $_SESSION["dati"]["username"];
