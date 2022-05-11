76bbebba57820b565f4254e58b6c27d0880b5174
capuanomaurizio <mauricapuano@gmail.com>
2022-05-11 11:40:30 +0200

    Aggiunta di un burger menu e relative funzionalità

--- home.php
+++ home.php
@@ -9,22 +9,57 @@
 <body>
     <div id="nav">
         <div id="navSx">
-            <div id="logo"></div>
+            <div id="logoContainer">
+                <div id="logo"></div>
+            </div>
         </div>
         <div id="navDx">
-            <div id="userDiv">&emsp;<i class="fa-solid fa-user icon"></i>&ensp;<?php echo $_SESSION["user"]?>&emsp;</div>
+            <div id="userDiv" onClick="toggleMenu();">&emsp;<i class="fa-solid fa-user icon"></i>&ensp;<?php echo $_SESSION["user"]?>&emsp;</div>
+            <div id="burgerMenu" data-status="closed">
+                <div id="uscita" class="burgers" onClick="openInfo();"><i class="fa-solid fa-circle-info"></i><div>&nbsp;&nbsp;Info</div></div>
+                <div id="uscita" class="burgers" onClick="window.location.href = './uscita.php';"><i class="fa-solid fa-arrow-right-from-bracket"></i><div>&nbsp;&nbsp;Uscita</div></div>
+            </div>
         </div>
     </div>
     <div id="body">
         <div id="bodySx">
-            <div id="bodyTopSx"></div>
-            <div id="bodyBotSx"></div>
+            <div id="bodyTopSx" class="bodyDivs"></div>
+            <div id="bodyBotSx" class="bodyDivs"></div>
         </div>
         <div id="bodyDx">
-            <div id="bodyTopDx"></div>
-            <div id="bodyBotDx"></div>
+            <div id="bodyTopDx" class="bodyDivs"></div>
+            <div id="bodyBotDx" class="bodyDivs"></div>
         </div>
     </div>
+    <div id="aboutContainer">
+        <div id="about">
+            <h1>MADE BY MAURIZIO CAPUANO</h1>
+        </div>
+    </div>
+    <script>
+        document.addEventListener('click', function(e) {
+            if(e.target.matches('#aboutContainer')) {
+                closeInfo();
+            }
+            if(!e.target.matches('#burgerMenu, #burgerMenu *, #userDiv, #userDiv *') && document.getElementById("burgerMenu").dataset.status == "open"){
+                toggleMenu();
+            }
+        })
+        function toggleMenu(){
+            if(document.getElementById("burgerMenu").dataset.status != "open"){
+                document.getElementById("burgerMenu").dataset.status = "open";
+            } else {
+                document.getElementById("burgerMenu").dataset.status = "closed";
+            }
+        }
+        function openInfo(){
+            document.getElementById("aboutContainer").style.display = "flex";
+            document.getElementById("burgerMenu").dataset.status = "closed";
+        }
+        function closeInfo(){
+            document.getElementById("aboutContainer").style.display = "none";
+        }
+    </script>
 </body>
 </html>
 
